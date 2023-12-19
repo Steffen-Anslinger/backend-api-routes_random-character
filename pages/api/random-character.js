607fb266d9c554f14296795ff711b316a7df9387
Steffen Anslinger <steffen.anslinger@hotmail.com>
2023-12-19 15:48:37 +0100

    finish challenge

---
+++ pages/api/random-character.js
@@ -0,0 +1,26 @@
+import Chance from "chance";
+
+const chance = new Chance();
+
+// if the variable is added outside the function one random character is displayed
+// if used inside the function on each refresh a new random character is displayed
+// const character = {
+//   prefix: chance.prefix(),
+//   firstName: chance.first(),
+//   lastName: chance.last(),
+//   age: chance.age(),
+//   twitterName: chance.twitter(),
+//   geohash: chance.geohash(),
+// };
+
+export default function handler(request, response) {
+  const character = {
+    prefix: chance.prefix(),
+    firstName: chance.first(),
+    lastName: chance.last(),
+    age: chance.age(),
+    twitterName: chance.twitter(),
+    geohash: chance.geohash(),
+  };
+  response.status(200).json(character);
+}
